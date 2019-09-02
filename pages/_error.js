00ff7a1eb7a771a35097945152b49aa9a7dfc606
plug-n-play <>
2019-09-02 22:08:22 +0530

    Added 404 page

---
+++ pages/_error.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import { useRouter } from 'next/router';
+
+const Error = () => {
+  const router = useRouter();
+  return (
+    <Layout>
+      <div className="error-message row" id="content-root">
+        <div>
+          <h1 id="error-code">404</h1>
+          <div id="error-msg">
+            These are not the Singhs you are looking for.
+          </div>
+          <div id="error-desc">
+            The requested URL "{router.asPath}" was not found on this server.
+          </div>
+        </div>
+        <div>
+          <img
+            src="/static/assets/images/404.png"
+            alt="Image of Sikh Man smiling at you"
+          />
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Error;
